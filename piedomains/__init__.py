cad97eec0bb4a61648938e7ad91c5c0f13a08a57
***** <gsood07@gmail.com>
2023-02-04 11:24:37 -0800

    Update __init__.py

--- piedomains/__init__.py
+++ piedomains/__init__.py
@@ -1,3 +1,5 @@
 from .domain import pred_shalla_cat
+from .domain import pred_shalla_cat_with_images
+from .domain import pred_shalla_cat_with_text
 
 __all__ = ["pred_shalla_cat", "pred_shalla_cat_with_text", "pred_shalla_cat_with_images"]
