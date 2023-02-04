dd75d93a63f4fb10f491dfaf3de860f2465a2b46
***** <gsood07@gmail.com>
2023-02-04 11:28:26 -0800

    add end line to conf + change import order for pep8

--- piedomains/scripts/screenshot.py
+++ piedomains/scripts/screenshot.py
@@ -1,9 +1,10 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
 import os
 from PIL import Image
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 driver = None
 
