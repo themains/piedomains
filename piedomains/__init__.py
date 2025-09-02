48d9e8ab8a6eb830fbe311bc64bb0608d27274ae
***** <721466+soodoku@users.noreply.github.com>
2025-09-02 12:30:54 +0200

    try once more

--- piedomains/__init__.py
+++ piedomains/__init__.py
@@ -1,24 +1,6 @@
-# New modern API (recommended)
 from .api import DomainClassifier, classify_domains
 
-# Legacy API (deprecated but maintained for backward compatibility)
-from .domain import pred_shalla_cat
-from .domain import pred_shalla_cat_with_images
-from .domain import pred_shalla_cat_with_text
-from .domain import pred_shalla_cat_archive
-from .domain import pred_shalla_cat_with_text_archive  
-from .domain import pred_shalla_cat_with_images_archive
-
 __all__ = [
-    # New API
     "DomainClassifier",
     "classify_domains",
-    
-    # Legacy API
-    "pred_shalla_cat", 
-    "pred_shalla_cat_with_text", 
-    "pred_shalla_cat_with_images",
-    "pred_shalla_cat_archive",
-    "pred_shalla_cat_with_text_archive",
-    "pred_shalla_cat_with_images_archive"
 ]
